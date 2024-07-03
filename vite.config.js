b17f0a48ddce3be46e8cd4068c79ba76fa625364
Nicolas97431 <nickogsm@gmail.com>
2024-07-03 19:24:00 +0400

    Ajout du chemin de base pour le déploiement GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/nom-du-repo/", // Remplacez 'nom-du-repo' par le nom de votre dépôt GitHub
   plugins: [react()],
-})
+});
